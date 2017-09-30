277f5a9f344d757641a6ab40442aae96e8f3f3a5
Damayanti Ghosh <damayantighosh2@gmail.com>
2017-09-29 21:25:45 -0700

    Add files via upload

---
+++ RemoveVowels.java
@@ -0,0 +1,41 @@
+//Remove the vowels from string
+//GE
+
+import java.util.* ;
+import java.lang.*;
+class RemoveVowels
+{
+	public static void remove(String s)
+	{
+		HashMap<Integer,Character> record = new HashMap<>();
+		String s2 = "aeuio";
+
+		for(char c:s2.toCharArray())
+		{
+			System.out.println(c);
+			if(record.containsValue(c) == false)
+				record.put(((int)c),c) ;
+				
+			
+		}
+	
+		for(int i=0;i<s.length();i++)
+		{
+			char c = s.charAt(i);
+		
+			if (record.containsValue(c))
+			{
+				String s3 = Character.toString(c);
+				s = s.replace(s3,"");
+			}
+		}
+		System.out.println(s); 
+	}
+
+
+	public static void main(String[] args)
+	{
+		String s = "welcome to geeksforgeeks";
+		remove(s);
+	}
+}
